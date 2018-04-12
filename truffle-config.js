9ad1141af0a7757e13e6e299ede31b181403c130
jason <scnu_wang@163.com>
2018-04-12 17:18:34 +0800

    添加OpenZeppelin组件

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
-module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+// 这个文件主要供Windows下开发使用
+
+// module.exports = {
+//   // See <http://truffleframework.com/docs/advanced/configuration>
+//   // to customize your Truffle configuration!
+// };
