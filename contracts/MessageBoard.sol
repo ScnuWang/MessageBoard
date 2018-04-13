d07bb7ea2d60a17e9decb3010df57dbf5e8393c5
ScnuWang <scnu_wang@163.com>
2018-04-13 18:32:14 +0800

    添加Zeppenlin-solidity模块

---
+++ contracts/MessageBoard.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.17;
+
+
+contract MessageBoardToken is StandardToken {
+
+  string public constant name = "MessageBoardToken"; // solium-disable-line uppercase
+  string public constant symbol = "MBT"; // solium-disable-line uppercase
+  uint8 public constant decimals = 5; // solium-disable-line uppercase
+
+  uint256 public constant INITIAL_SUPPLY = 52.01314 * (10 ** uint256(decimals));
+
+  /**
+   * @dev Constructor that gives msg.sender all of existing tokens.
+   */
+  function MessageBoardToken() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+    Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+  }
+
+
+
+}
+
+
+
+
+
+
+
+contract MessageBoard {
+  /*
+      Token 基于 ERC20
+
+
+   */
+}
