30154e2de02d5cb69052d3c1a64387ca6a5dad42
ScnuWang <scnu_wang@163.com>
2018-04-19 19:12:03 +0800

    删除引入index.css

--- truffle.js
+++ truffle.js
@@ -1,5 +1,5 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "claw neither hedgehog pulse cinnamon become rally rally pistol love since now"; 
+var mnemonic = "claw neither hedgehog pulse cinnamon become rally rally pistol love since now";
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
