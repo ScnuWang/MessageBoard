81df7c23f98174afcc5303d436eadcedb01c8709
ScnuWang <scnu_wang@163.com>
2018-04-19 16:06:36 +0800

    初步完成留言板功能：留言、查询留言

--- contracts/MessageBoard.sol
+++ contracts/MessageBoard.sol
@@ -7,30 +7,26 @@ contract MessageBoard {
    */
    /// @dev 留言信息结构体
    struct Message {
-       uint dateTime;   // 留言的时间
-       string nickName; // 留言的昵称
+       address account;  // 留言者的地址
        string content;  // 留言的内容
    }
 
    // 通过地址查找留言
    mapping(address => Message) public messages;
 
-    address[] accounts;
+   address[] accounts;
 
    /// @dev 添加留言
-   function setMessage (address _account,string _nickName, string _content) public {
+   function setMessage (address _account,string _content) public {
       Message message = messages[_account];
-      message.dateTime = now;
-      message.nickName = _nickName;
       message.content  = _content;
-
+      message.account = _account;
       accounts.push(_account) -1;
    }
 
    /// @dev 获取留言
    function getMessageContent (address _account) public view returns(string) {
       Message message = messages[_account];
-
       return message.content;
    }
 
--- src/js/app.js
+++ src/js/app.js
@@ -17,7 +17,7 @@ App = {
     }
     //这里的 web3 是否需要调用，在哪里调用的？ 应该暂时还没有调用的
     web3 = new Web3(App.web3Provider);
-
+    console.log(web3);
     return App.initContract();
   },
 
@@ -30,12 +30,9 @@ App = {
       console.log(MessageBoardArtifact);
 
       App.contracts.MessageBoard = TruffleContract(MessageBoardArtifact);
-
       // 为合约提供Provider
       App.contracts.MessageBoard.setProvider(App.web3Provider);
 
-      // 使用我们的合同来检索和标记养宠物
-      // return App.markLeaveMessage();
     });
 
     return App.bindEvents();
@@ -43,6 +40,7 @@ App = {
 
   bindEvents: function() {
     $(document).on('click', '#leavemessage', App.handleLeaveMessage);
+    $(document).on('click', '#querymessage', App.handleQueryMessage);
   },
 
 
@@ -51,22 +49,63 @@ App = {
 
     var messageBoardInstance;
 
-    var nickName = $('#nickname').val();
-    console.log("昵称： "+nickName);
+    MessageFrom = $('#leavemessageaccount').val();
+    console.log("账户： "+MessageFrom);
     var content = $('#content').val();
     console.log("内容： "+content);
     web3.eth.getAccounts(function(error, accounts) {
       if (error) {
         console.log(error);
       }
+      console.log("所有的账户： "+accounts);
+      // 如果指定了账户就使用指定账户留言，否则使用默认第一个账户
+      var account;
+      if(!MessageFrom){
+        account = accounts[0];
+      }else {
+        account = MessageFrom;
+      }
+      console.log("=====>"+account);
+      App.contracts.MessageBoard.deployed().then(function(instance) {
+        messageBoardInstance = instance;
+        return messageBoardInstance.setMessage(account,content);
+      }).then(function(instance){
+        alert("留言成功！！！");
+      }).catch(function(err) {
+        alert("出现异常，请联系管理员：scnu_wang@163.com")
+        console.log(err.message);
+      });
+    });
+  },
 
-      var account = accounts[0];
+  handleQueryMessage: function(event) {
+    event.preventDefault();
+
+    var messageBoardInstance;
+
+    MessageFrom = $('#querymessageaccount').val();
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account;
+      if(!MessageFrom){
+        account = accounts[0];
+      }else {
+        account = MessageFrom;
+      }
 
       App.contracts.MessageBoard.deployed().then(function(instance) {
         messageBoardInstance = instance;
-        // Execute adopt as a transaction by sending account
-        return messageBoardInstance.setMessage(account,nickName,content);
-      }).catch(function(err) {
+        console.log(instance);
+        return messageBoardInstance.getMessageContent(account);
+      }).then(function(result) {
+        console.log(result);
+        $('#MessageBoard').text("查询结果： "+result);
+    }).catch(function(err) {
+        alert("出现异常！！！")
         console.log(err.message);
       });
     });
